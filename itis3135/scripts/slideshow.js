431022d19322179c70d7524b05a93870f1b1ad35
selfmadepeter1 <Peterayale@gmail.com>
2025-04-21 19:20:46 -0400

    p

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,17 +1,35 @@
-let slideIndex = 0;
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
 function showSlides(n) {
+  let i;
   const slides = document.getElementsByClassName("mySlides");
-  for (let i = 0; i < slides.length; i++) {
+  const dots = document.getElementsByClassName("demo");
+  const captionText = document.getElementById("caption");
+
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+
+  // Hide all slides
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[n].style.display = "block";
-  slideIndex = n;
+
+  // Remove active class from all thumbnails
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  // Show selected slide
+  slides[slideIndex - 1].style.display = "block";
+
+  // Highlight thumbnail
+  dots[slideIndex - 1].className += " active";
+
+  // Set caption text
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
-
-function showSlide(n) {
-  showSlides(n);
-}
-
-
-showSlides(slideIndex);
