e9b86ffe20c26ddd40321663792d89273ae677dd
selfmadepeter1 <Peterayale@gmail.com>
2025-05-03 23:35:38 -0400

    p

---
+++ itis3135/scripts/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+    background(220);
+  }
+  
+  function draw() {
+    background(255);
+  
+    fill(0);
+    textSize(200);
+    textFont('Georgia');
+    text('G', 120, 270);
+  }
