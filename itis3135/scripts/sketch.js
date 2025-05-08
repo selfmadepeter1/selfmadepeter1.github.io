6ae97306ff722ed779d96de28767d4d049250114
selfmadepeter1 <Peterayale@gmail.com>
2025-05-07 22:56:31 -0400

    p

--- itis3135/scripts/sketch.js
+++ itis3135/scripts/sketch.js
@@ -1,13 +1,30 @@
+let bgColor;
+
 function setup() {
-    createCanvas(400, 400);
-    background(220);
-  }
+  createCanvas(500, 500);
+  bgColor = color(255);
+  textFont('Georgia');
+  textSize(24);
+}
+
+function draw() {
+  background(bgColor);
+
   
-  function draw() {
-    background(255);
+  fill(0, 100, 200);
+  ellipse(mouseX, mouseY, 50, 50);
+
   
-    fill(0);
-    textSize(200);
-    textFont('Georgia');
-    text('G', 120, 270);
-  }
+  fill(50);
+  text("Follow me!", mouseX + 10, mouseY - 10);
+}
+
+
+function mousePressed() {
+  bgColor = color(random(255), random(255), random(255));
+
+  // Also draw a circle on click
+  fill(random(255), random(255), random(255));
+  noStroke();
+  ellipse(mouseX, mouseY, 40, 40);
+}
